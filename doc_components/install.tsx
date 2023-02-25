349b7675b0e85182f815601a972f607c12cc1d66
siduck <siduck@tutanota.com>
2023-02-25 13:28:42 +0530

    add tabs ( os selector ) in installer page

--- doc_components/install.tsx
+++ doc_components/install.tsx
@@ -1,11 +1,44 @@
 import { createSignal } from "solid-js";
 
-const [txt, setxt] = createSignal("abc");
+const [osname, setOS] = createSignal("unix");
+
+// installer commands
+const unix_cmd =
+  "git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim";
+
+const windows_cmd =
+  "git clone https://github.com/NvChad/NvChad $HOME\\AppData\\Local\\nvim --depth 1 && nvim";
+
+const [oscmd, setOSCMD] = createSignal(unix_cmd);
 
 export default () => {
   return (
-    <button p-3 rounded-2 w-fit onclick={() => setxt("hi")}>
-      {txt}
-    </button>
+    <div grid gap-5>
+      <div class="[&_*]:rounded-lg [&_*]:p-4  [&_*]:w-fit vertCentered !gap-5">
+        {/* unix btn */}
+        <button
+          bg={osname() == "unix" ? "blue-2" : ""}
+          onclick={() => {
+            setOS("unix");
+            setOSCMD(unix_cmd);
+          }}
+        >
+          Linux / MacOS
+        </button>
+
+        {/* windows btn */}
+        <button
+          bg={osname() == "windows" ? "blue-2" : ""}
+          onclick={() => {
+            setOS("windows");
+            setOSCMD(windows_cmd);
+          }}
+        >
+          Windows
+        </button>
+      </div>
+
+      <pre class="hljs">{oscmd}</pre>
+    </div>
   );
 };
