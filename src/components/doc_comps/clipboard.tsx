cdc8bad5bc082acc512e64abc3baf19f6d4b7559
siduck <siduck@tutanota.com>
2023-03-13 22:29:19 +0530

    improve accesibility score on lighthouse

--- src/components/doc_comps/clipboard.tsx
+++ src/components/doc_comps/clipboard.tsx
@@ -5,6 +5,7 @@ export default (id: string) => {
   preElements.forEach(function (preElement) {
     const button = document.createElement("button");
     button.classList = "copyBtn";
+    button.ariaLabel = "copy button"
 
     // create copy icon
     const icon = document.createElement("div");
