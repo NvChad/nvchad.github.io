e20aa452f15bc2fe3310bd9c06a63d764152a1da
Nyi Nyi Lwin <mdy.nnl@gmail.com>
2023-03-24 14:28:38 +0630

    remove unnecessary files

--- src/routes/(index)/news/[...path].tsx
+++ src/routes/(index)/news/[...path].tsx
@@ -1,7 +1,7 @@
 import { Dynamic } from "solid-js/web";
 import { useLocation } from "solid-start";
 import { newsMap } from "~/components/news_comps/newsData";
-import { NewsPage } from "~/components/news_comps/newsRoutes";
+import NewsPage from "~/components/news_comps/NewsPage";
 
 export default function NewsRoutes() {
   const location = useLocation();
