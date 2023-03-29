02e60c71ce09e3cbe0e514200a33643fdc0d102c
siduck <siduck@users.noreply.github.com>
2023-03-29 23:00:37 +0000

    deploy: 306abb337598d5d854d1d37f144f8eb40d467cc2

--- assets/post-install-a9cb1dc2.js
+++ assets/post-install-a9cb1dc2.js
@@ -1,4 +1,4 @@
-import{M as i}from"./index-d16430d5.js";import{b as n,q as t}from"./entry-client-29c1bbcd.js";function r(o){const e=Object.assign({h2:"h2",p:"p",ul:"ul",li:"li",a:"a"},i(),o.components);return[n(e.h2,{children:"If you're new to Neovim/Vim"}),`
+import{M as i}from"./index-22b1818d.js";import{b as n,q as t}from"./entry-client-3cb49ff2.js";function r(o){const e=Object.assign({h2:"h2",p:"p",ul:"ul",li:"li",a:"a"},i(),o.components);return[n(e.h2,{children:"If you're new to Neovim/Vim"}),`
 `,n(e.p,{children:"We strongly encourage you to learn how to use Neovim/Vim. If you are totally new to Vim then you cannot use NvChad until you gain some basic knowledge about Vim's modes, default editor commands, globals, mappings etc."}),`
 `,n(e.p,{children:"These are highly recommended and a must do for any new Vimmer:"}),`
 `,n(e.ul,{get children(){return[`
