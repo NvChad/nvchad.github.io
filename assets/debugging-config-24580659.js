bce92fd3af5c467a949be49b80a58e3c77c7cddb
siduck <siduck@users.noreply.github.com>
2023-04-02 23:46:59 +0000

    deploy: e1d5c1155157927f08a392712106b0158438a1c6

--- assets/debugging-config-24580659.js
+++ assets/debugging-config-24580659.js
@@ -1,4 +1,4 @@
-import{M as r}from"./index-f97cef65.js";import{b as n,q as o}from"./entry-client-debd7ce2.js";function i(t){const e=Object.assign({h2:"h2",ul:"ul",li:"li",code:"code",a:"a"},r(),t.components);return[n(e.h2,{children:"Basic debugging"}),`
+import{M as r}from"./index-1b4c6efe.js";import{b as n,q as o}from"./entry-client-de76ba16.js";function i(t){const e=Object.assign({h2:"h2",ul:"ul",li:"li",code:"code",a:"a"},r(),t.components);return[n(e.h2,{children:"Basic debugging"}),`
 `,n(e.ul,{get children(){return[`
 `,n(e.li,{get children(){return["You can check the health of Neovim with the ",n(e.code,{children:"checkhealth"})," command."]}}),`
 `,n(e.li,{children:"Make sure you dont have syntax errors in your custom config files"}),`
---
+++ assets/syntax-444976fc.js
@@ -0,0 +1,41 @@
+import{b as n,q as l,g as a,t as i}from"./entry-client-de76ba16.js";import{M as r}from"./index-1b4c6efe.js";const c=i("<br>",1);function t(s){const e=Object.assign({h1:"h1",p:"p",a:"a",h2:"h2",code:"code",ul:"ul",li:"li",pre:"pre",span:"span"},r(),s.components);return[n(e.h1,{children:"Nvim-treesitter"}),`
+`,n(e.p,{get children(){return["We use ",n(e.a,{href:"https://github.com/nvim-treesitter/nvim-treesitter",children:"Nvim-treesitter"})," plugin to have proper syntax highlighting in NvChad. It can be used for various things such as auto indent etc too."]}}),`
+`,n(e.h2,{children:"Install parsers"}),`
+`,n(e.p,{get children(){return["The TSInstall command is used to install treesitter parsers i.e ",n(e.code,{children:"TSInstall <parser>"})]}}),`
+`,n(e.ul,{get children(){return[`
+`,n(e.li,{children:"Example :"}),`
+`]}}),`
+`,n(e.pre,{get children(){return n(e.code,{className:"hljs language-lua",children:`TSInstall lua html
+`})}}),`
+`,a(c),`
+`,n(e.p,{children:"But this may be tedious when you have so many parsers to install and you'd have to repeat this step if you're re-installing nvchad with your old custom settings."}),`
+`,n(e.h2,{children:"Custom config"}),`
+`,n(e.ul,{get children(){return[`
+`,n(e.li,{children:"So now we'll just override the default config and add our own parser names to it."}),`
+`,n(e.li,{get children(){return["For knowing correct parser names, do check ",n(e.a,{href:"https://github.com/nvim-treesitter/nvim-treesitter#supported-languages",children:"nvim-treesitter docs"})]}}),`
+`,n(e.li,{get children(){return n(e.code,{children:"custom/plugins.lua"})}}),`
+`]}}),`
+`,n(e.pre,{get children(){return n(e.code,{className:"hljs language-lua",get children(){return[`{
+    `,n(e.span,{className:"hljs-string",children:'"nvim-treesitter/nvim-treesitter"'}),`,
+    opts = {
+      ensure_installed = {
+        `,n(e.span,{className:"hljs-comment",children:"-- defaults "}),`
+        `,n(e.span,{className:"hljs-string",children:'"vim"'}),`,
+        `,n(e.span,{className:"hljs-string",children:'"lua"'}),`,
+
+        `,n(e.span,{className:"hljs-comment",children:"-- web dev "}),`
+        `,n(e.span,{className:"hljs-string",children:'"html"'}),`,
+        `,n(e.span,{className:"hljs-string",children:'"css"'}),`,
+        `,n(e.span,{className:"hljs-string",children:'"javascript"'}),`,
+        `,n(e.span,{className:"hljs-string",children:'"typescript"'}),`,
+        `,n(e.span,{className:"hljs-string",children:'"tsx"'}),`,
+        `,n(e.span,{className:"hljs-string",children:'"json"'}),`,
+        `,n(e.span,{className:"hljs-comment",children:'-- "vue", "svelte",'}),`
+
+       `,n(e.span,{className:"hljs-comment",children:"-- low level"}),`
+        `,n(e.span,{className:"hljs-string",children:'"c"'}),`,
+        `,n(e.span,{className:"hljs-string",children:'"zig"'}),`
+      },
+    },
+  },
+`]}})}})]}function d(s={}){const{wrapper:e}=Object.assign({},r(),s.components);return e?n(e,l(s,{get children(){return n(t,s)}})):t(s)}export{d as default};
