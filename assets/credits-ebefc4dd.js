d87ac9316579ef578238aa04d1b3db450f98c671
siduck <siduck@users.noreply.github.com>
2023-04-05 01:55:54 +0000

    deploy: b85b226a116ee361fb1623b465c1d0c526816ab7

--- assets/credits-ebefc4dd.js
+++ assets/credits-ebefc4dd.js
@@ -1,4 +1,4 @@
-import{M as i}from"./index-3cccd78f.js";import{b as n,q as h}from"./entry-client-4a4b9c0c.js";function r(t){const e=Object.assign({p:"p",ul:"ul",li:"li",a:"a",strong:"strong"},i(),t.components);return[n(e.p,{children:"The NvChad team would love to acknowledge many projects which made this possible."}),`
+import{M as i}from"./index-78ccf408.js";import{b as n,q as h}from"./entry-client-68e3e989.js";function r(t){const e=Object.assign({p:"p",ul:"ul",li:"li",a:"a",strong:"strong"},i(),t.components);return[n(e.p,{children:"The NvChad team would love to acknowledge many projects which made this possible."}),`
 `,n(e.p,{children:"Thank you!"}),`
 `,n(e.ul,{get children(){return[`
 `,n(e.li,{get children(){return[`
