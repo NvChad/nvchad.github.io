f9c1cbbf3b7f0e94a9102f85dd2b70758d29a9be
siduck <siduck@tutanota.com>
2023-09-30 22:15:39 +0530

    redirect /docs route to install page

---
+++ src/routes/(index)/docs/index.jsx
@@ -0,0 +1,13 @@
+import { onMount } from "solid-js";
+import { useNavigate } from "@solidjs/router";
+
+const Docs = () => {
+	onMount(() => {
+		const navigate = useNavigate();
+		navigate("/docs/quickstart/install", { replace: true });
+	});
+
+	return <></>;
+};
+
+export default Docs;
