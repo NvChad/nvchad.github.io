0458f60fb27249c260fe047945b42548e7fb0391
siduck <siduck@tutanota.com>
2024-02-17 20:11:37 +0530

    add some routes

---
+++ src/routes/news.tsx
@@ -0,0 +1,56 @@
+import { createEffect, onCleanup, onMount } from "solid-js";
+
+import ContextTitles from "~/components/ContextTitles";
+
+import {
+  assign_heading_ids,
+  autoscroll_toID,
+  contextHeadings,
+  generateActiveContext,
+} from "~/utils";
+
+const Layout= (props) =>{
+  onMount(() => {
+    const el = document.getElementById("newsContent");
+    const imgs = el?.querySelectorAll("img");
+
+    if (imgs) {
+      imgs.forEach((img, index) => {
+        // skip lazyloading first img
+        if (index != 0) {
+          img.setAttribute("loading", "lazy");
+        }
+      });
+    }
+
+    const onScroll = () => generateActiveContext("newsContent");
+    window.addEventListener("scroll", onScroll);
+
+    onCleanup(() => window.removeEventListener("scroll", onScroll));
+  });
+
+  //  run on route change
+  createEffect(
+    () => {
+      setTimeout(() => {
+        // create_copyIcon("DocContent");
+        assign_heading_ids();
+        generateActiveContext("newsContent");
+        autoscroll_toID();
+      }, 50);
+    },
+  );
+
+  return (
+    <div
+      p="5 xl:10"
+      class="box mx-auto flex flex-col-reverse xl:grid xl:grid-cols-[1fr_auto]"
+    >
+      <div id="newsContent">{props.children}</div>
+
+      {/* on this page component */}
+      {contextHeadings.length > 1 && <ContextTitles />}
+    </div>
+  );
+}
+export default Layout;
---
+++ src/routes/news/index.jsx
@@ -0,0 +1,63 @@
+import { A } from "@solidjs/router";
+
+const toMeta = (v) =>
+  Object.entries(v).map(([link, mod]) => ({
+    link: link.replace(/^\.\/|\.mdx$/g, ""),
+    ...mod.meta,
+  }));
+
+const news = toMeta(import.meta.glob("./*.mdx", { eager: true }));
+
+console.log(news)
+
+function News() {
+  return (
+    <div m="y-5 xl:y-10 x-auto" px-3 max="w-[1700px]">
+      {/* overview cards */}
+      <div grid gap-5 class="md:grid-cols-2 2xl:grid-cols-3">
+        {news.map((x) => {
+          /* card */
+          return (
+            <div
+              border="slate 0 dark:dark-4 solid"
+              class="shadow-xl flex flex-col  gap-4 items-start"
+              bg="dark:dark-3"
+            >
+              <img
+                src={`/news/${x.cover}`}
+                w-full
+                rounded-t-lg
+                loading="lazy"
+              />
+
+              <div h-full flex flex-col gap-10 justify-between p-10 pt-5>
+                <div>
+                  <h2 class="m-0" pb-5>
+                    {x.heading}
+                  </h2>
+                  <p text-lg class="m-0 p-0">
+                    {x.details}
+                  </p>
+                </div>
+
+                <A href={x.link}>
+                  <button
+                    bg-blue-6
+                    text-white-1
+                    dark:bg-blue-3
+                    dark:text-dark-1
+                    px-5
+                  >
+                    Read More
+                  </button>
+                </A>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default News;
