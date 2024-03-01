a2c4aec9974970c1c4224b8a8a59d9db208fa971
siduck <siduck@tutanota.com>
2024-03-01 14:05:11 +0000

    Deploying to gh-pages from @ NvChad/nvchad.github.io@2b9f93ff2467ebaf473c6f63c344834407fcf66e 🚀

--- _build/assets/faq-CAURBtj-.js
+++ _build/assets/faq-CAURBtj-.js
@@ -1,4 +1,4 @@
-import{M as o}from"./index-CYDpJFld.js";import{b as n,m as i}from"./web-DScAkgcG.js";function t(r){const e={a:"a",code:"code",h2:"h2",li:"li",p:"p",pre:"pre",span:"span",ul:"ul",...o(),...r.components};return[n(e.h2,{children:"Icons not working"}),`
+import{M as i}from"./index-CYDpJFld.js";import{b as n,m as o}from"./web-DScAkgcG.js";function t(r){const e={a:"a",code:"code",h2:"h2",li:"li",p:"p",pre:"pre",span:"span",strong:"strong",ul:"ul",...i(),...r.components};return[n(e.h2,{children:"Icons not working"}),`
 `,n(e.ul,{get children(){return[`
 `,n(e.li,{get children(){return["Setup a ",n(e.a,{href:"https://www.nerdfonts.com/#home",children:"nerdfont"})," as your terminal's default font."]}}),`
 `]}}),`
@@ -25,13 +25,22 @@ import{M as o}from"./index-CYDpJFld.js";import{b as n,m as i}from"./web-DScAkgcG
 `,n(e.p,{get children(){return["In Nvchad ",n(e.code,{children:"<Tab> & <S-Tab>"})," are mapped by default and nvim maps to ",n(e.code,{children:"<C-i>"})," if ",n(e.code,{children:"<Tab>"})," key is mapped. ",n(e.a,{href:"https://stackoverflow.com/questions/16209213/vim-jump-forward-with-c-i-doesnt-work-for-me-too-snipmate/16209534#16209534",children:"Check"})]}}),`
 `]}}),`
 `,n(e.li,{get children(){return[`
-`,n(e.p,{get children(){return["To map those keys, you should either disable ",n(e.code,{children:"<Tab>"})," in your custom mappings file or have certain settings in your terminal config, for exampele:  alacritty config :"]}}),`
+`,n(e.p,{get children(){return["To map those keys, you should either disable ",n(e.code,{children:"<Tab>"})," in your custom mappings file or have certain settings in your terminal config, for example:  alacritty config :"]}}),`
 `]}}),`
 `]}}),`
 `,n(e.pre,{get children(){return n(e.code,{className:"hljs language-bash",get children(){return[`key_bindings:
    - { key: I, mods: Control, chars: `,n(e.span,{className:"hljs-string",children:'"\\x1b[105;6u"'}),` }
 `]}})}}),`
+`,n(e.ul,{get children(){return[`
+`,n(e.li,{get children(){return[n(e.strong,{children:"NOTE"}),": keymaps in NvChad are case sensitive, to delete it you need to use lowercase ",n(e.code,{children:"<tab>"}),". Add this to your mappings.lua:"]}}),`
+`]}}),`
+`,n(e.pre,{get children(){return n(e.code,{className:"hljs language-lua",get children(){return[`M.disabled = {
+	n = {
+		[`,n(e.span,{className:"hljs-string",children:'"<tab>"'}),"] = ",n(e.span,{className:"hljs-string",children:'""'}),`,
+	},
+}
+`]}})}}),`
 `,n(e.h2,{children:"NvChad's color is weird"}),`
 `,n(e.ul,{get children(){return[`
 `,n(e.li,{get children(){return["You might be using a terminal that doesn't support true color. This problem is very common for those who use the default MacOS terminal, opt to use ",n(e.a,{href:"https://iterm2.com/",children:"iterm2"})," instead."]}}),`
-`]}})]}function l(r={}){const{wrapper:e}={...o(),...r.components};return e?n(e,i(r,{get children(){return n(t,r)}})):t(r)}export{l as default};
+`]}})]}function l(r={}){const{wrapper:e}={...i(),...r.components};return e?n(e,o(r,{get children(){return n(t,r)}})):t(r)}export{l as default};
