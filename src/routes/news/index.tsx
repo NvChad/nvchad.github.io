2bd027ea3c258a426202ec83400e7335eb6724fe
siduck <siduck@tutanota.com>
2024-03-09 21:26:45 +0530

    fix some meta descs

--- src/routes/news/index.tsx
+++ src/routes/news/index.tsx
@@ -1,6 +1,6 @@
 const toMeta = (v) =>
   Object.entries(v).map(([link, mod]) => ({
-    link: link.replace(/^\.\/|\.mdx$/g, ""),
+    link: "/news/" + link.replace(/^\.\/|\.mdx$/g, ""),
     ...mod.meta,
   }));
 
@@ -16,46 +16,38 @@ function News() {
     <div m="y-5 xl:y-10 x-auto" px-3 max="w-[1700px]">
       {/* overview cards */}
       <div grid gap-5 class="md:grid-cols-2 2xl:grid-cols-4">
-        {news.map((x) => {
-          /* card */
-          return (
-            <div
-              border="slate 0 dark:dark-4 solid"
-              class="shadow-xl flex flex-col  gap-4 items-start"
-              bg="dark:dark-3"
-            >
-              <img
-                src={`/news/${x.cover}`}
-                w-full
-                rounded-t-lg
-                loading="lazy"
-              />
+        {news.map((x) => (
+          <div
+            border="slate 0 dark:dark-4 solid"
+            class="shadow-lg flex flex-col  gap-4 items-start"
+            bg="dark:dark-3"
+          >
+            <img src={`/news/${x.cover}`} w-full rounded-t-lg loading="lazy" />
 
-              <div h-full flex flex-col gap-4 justify-between p-10 pt-5>
-                <div>
-                  <h2 class="m-0" pb-5>
-                    {x.title}
-                  </h2>
-                  <p text-lg class="m-0 p-0">
-                    {x.desc}
-                  </p>
-                </div>
-
-                <a href={x.link}>
-                  <button
-                    bg-blue-6
-                    text-white-1
-                    dark:bg-blue-3
-                    dark:text-dark-1
-                    px-5
-                  >
-                    Read More
-                  </button>
-                </a>
+            <div h-full flex flex-col gap-4 justify-between p-10 pt-5>
+              <div>
+                <h2 class="m-0" pb-5>
+                  {x.title}
+                </h2>
+                <p text-lg class="m-0 p-0">
+                  {x.desc}
+                </p>
               </div>
+
+              <a href={x.link}>
+                <button
+                  bg-blue-6
+                  text-white-1
+                  dark:bg-blue-3
+                  dark:text-dark-1
+                  px-5
+                >
+                  Read More
+                </button>
+              </a>
             </div>
-          );
-        })}
+          </div>
+        ))}
       </div>
     </div>
   );
