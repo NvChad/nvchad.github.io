5a9b2495b67c7d3ebef58fef44c69d722dc19a1a
siduck <siduck@tutanota.com>
2024-03-21 05:26:29 +0000

    Deploying to gh-pages from @ NvChad/nvchad.github.io@ecc60806e1a4609411340d763f0c8f1c3c6b3d46 🚀

--- _build/assets/install-Dj6hC_KC.js
+++ _build/assets/install-Dj6hC_KC.js
@@ -1,4 +1,4 @@
-import{b as e,q as s,k as t,t as l}from"./web-CBU83r8B.js";import{M as a}from"./index-C2YDUEaq.js";import{O as o}from"./install-DI_Cow-H.js";import"./clipboard-DzJ4oxno.js";var c=l("<strong>Mono"),d=l("<strong>Example : "),h=l("<s>JetbrainsMono Nerd Font Mono"),m=l("<strong>(OPTIONAL)"),u=l("<br>");const w={title:"NvChad Installation",desc:"Install, Update, Uninstall guide for nvchad"};function i(r){const n={a:"a",code:"code",h2:"h2",li:"li",pre:"pre",span:"span",strong:"strong",ul:"ul",...a(),...r.components};return[e(n.h2,{children:"Pre-requisites"}),`
+import{b as e,q as o,k as t,t as l}from"./web-CBU83r8B.js";import{M as a}from"./index-C2YDUEaq.js";import{O as s}from"./install-DI_Cow-H.js";import"./clipboard-DzJ4oxno.js";var c=l("<strong>Mono"),d=l("<strong>Example : "),h=l("<s>JetbrainsMono Nerd Font Mono"),m=l("<strong>(OPTIONAL)"),u=l("<br>");const w={title:"NvChad Installation",desc:"Install, Update, Uninstall guide for nvchad"};function i(r){const n={a:"a",code:"code",h2:"h2",li:"li",pre:"pre",span:"span",strong:"strong",ul:"ul",...a(),...r.components};return[e(n.h2,{children:"Pre-requisites"}),`
 `,e(n.ul,{get children(){return[`
 `,e(n.li,{get children(){return[e(n.a,{href:"https://github.com/neovim/neovim/releases/tag/v0.9.5",children:"Neovim 0.9.5"}),"."]}}),`
 `,e(n.li,{get children(){return[e(n.a,{href:"https://www.nerdfonts.com/",children:"Nerd Font"})," as your terminal font.",`
@@ -13,10 +13,11 @@ import{b as e,q as s,k as t,t as l}from"./web-CBU83r8B.js";import{M as a}from"./
 `,e(n.li,{children:"Delete old neovim folders (check commands below)"}),`
 `]}}),`
 `,e(n.h2,{children:"Install"}),`
-`,e(o,{}),`
+`,e(s,{}),`
 `,t(u),`
 `,e(n.ul,{get children(){return[`
 `,e(n.li,{get children(){return["Run ",e(n.code,{children:":MasonInstallAll"})," command after lazy.nvim finishes downloading plugins."]}}),`
+`,e(n.li,{get children(){return["Delete the ",e(n.code,{children:".git"})," folder from nvim folder."]}}),`
 `]}}),`
 `,e(n.h2,{children:"Update"}),`
 `,e(n.ul,{get children(){return[`
@@ -34,4 +35,4 @@ rd -r ~\\AppData\\Local\\nvim-data
 `,e(n.span,{className:"hljs-comment",children:"# Window PowerShell"}),`
 `,e(n.span,{className:"hljs-built_in",children:"rm"}),` -Force ~\\AppData\\Local\\nvim
 `,e(n.span,{className:"hljs-built_in",children:"rm"}),` -Force ~\\AppData\\Local\\nvim-data
-`]}})}})]}function N(r={}){const{wrapper:n}={...a(),...r.components};return n?e(n,s(r,{get children(){return e(i,r)}})):i(r)}export{N as default,w as meta};
+`]}})}})]}function N(r={}){const{wrapper:n}={...a(),...r.components};return n?e(n,o(r,{get children(){return e(i,r)}})):i(r)}export{N as default,w as meta};
