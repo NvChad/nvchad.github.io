1dabe438bebd5944a6cd0673322801d1c3455849
siduck <siduck@tutanota.com>
2024-07-07 12:16:24 +0000

    Deploying to gh-pages from @ NvChad/nvchad.github.io@ffc1dac95b3d07db578d6a1daaf432499167ca04 🚀

--- _build/assets/lsp-rle2oKe4.js
+++ _build/assets/lsp-rle2oKe4.js
@@ -1,4 +1,4 @@
-import{b as e,q as o,k as a,t as c,m as h}from"./web-uzQsy4d_.js";import{M as t}from"./index-BosUlz4E.js";var r=c("<br>"),d=c("<u>");const g={title:"NvChad Lsp Configuration",desc:"Manage lsp servers & mason.nvim in NvChad"};function i(s){const n={a:"a",blockquote:"blockquote",code:"code",h2:"h2",li:"li",p:"p",pre:"pre",span:"span",strong:"strong",ul:"ul",...t(),...s.components};return[e(n.h2,{children:"Setup lsp server"}),`
+import{b as e,q as o,k as l,t as r,m as h}from"./web-uzQsy4d_.js";import{M as t}from"./index-BosUlz4E.js";var c=r("<br>"),m=r("<u>");const u={title:"NvChad Lsp Configuration",desc:"Manage lsp servers & mason.nvim in NvChad"};function i(s){const n={a:"a",blockquote:"blockquote",code:"code",h2:"h2",li:"li",p:"p",pre:"pre",span:"span",strong:"strong",ul:"ul",...t(),...s.components};return[e(n.h2,{children:"Setup lsp server"}),`
 `,e(n.p,{get children(){return["Before starting, it is strongly recommended that you walk through the LSP configuration ",e(n.a,{href:"https://github.com/neovim/nvim-lspconfig",children:"lspconfig repository"}),"."]}}),`
 `,e(n.p,{get children(){return["Then check ",e(n.a,{href:"https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md",children:"server_configurations.md"})," to make sure your language's LSP server is present there."]}}),`
 `,e(n.ul,{get children(){return[`
@@ -13,7 +13,7 @@ import{b as e,q as o,k as a,t as c,m as h}from"./web-uzQsy4d_.js";import{M as t}
    `,e(n.span,{className:"hljs-keyword",children:"end"}),`,
 },
 `]}})}}),`
-`,a(r),`
+`,l(c),`
 `,e(n.ul,{get children(){return[`
 `,e(n.li,{get children(){return e(n.strong,{children:"configs/lspconfig.lua"})}}),`
 `]}}),`
@@ -47,21 +47,8 @@ import{b as e,q as o,k as a,t as c,m as h}from"./web-uzQsy4d_.js";import{M as t}
 `,e(n.span,{className:"hljs-comment",children:"-- }"}),`
 `]}})}}),`
 `,e(n.h2,{children:"Mason.nvim"}),`
-`,e(n.p,{get children(){return["The ",e(n.code,{children:"mason.nvim"})," plugin is used to download LSP servers, formatters, linters, and debug adapters. It's better to list all your required packages in your Mason override config, so they automatically download when running ",e(n.code,{children:"MasonInstallAll"})," command."]}}),`
-`,e(n.p,{get children(){return["You can find the exact name of the LSP packages using ",e(n.code,{children:":Mason"}),", that will open a window."]}}),`
-`,e(n.pre,{get children(){return e(n.code,{className:"hljs language-lua",get children(){return[` {
-   `,e(n.span,{className:"hljs-string",children:'"williamboman/mason.nvim"'}),`,
-   opts = {
-      ensure_installed = {
-        `,e(n.span,{className:"hljs-string",children:'"lua-language-server"'}),`,
-        `,e(n.span,{className:"hljs-string",children:'"html-lsp"'}),`,
-        `,e(n.span,{className:"hljs-string",children:'"prettier"'}),`,
-        `,e(n.span,{className:"hljs-string",children:'"stylua"'}),`
-      },
-    },
-  }
-`]}})}}),`
-`,a(r),`
+`,e(n.p,{get children(){return["Run the ",e(n.code,{children:"MasonInstallAll"})," command"]}}),`
+`,l(c),`
 `,e(n.blockquote,{get children(){return[`
-`,e(n.p,{get children(){return["Once the binaries are installed, you will have to configure them to properly work with LSP, conform.nvim, nvim-lint, nvim-dap etc. It depends on what you installed. ",(()=>{var l=a(d);return h(l,e(n.strong,{children:"NvChad does not provide any language configuration aside from lua"})),l})(),"."]}}),`
-`]}})]}function u(s={}){const{wrapper:n}={...t(),...s.components};return n?e(n,o(s,{get children(){return e(i,s)}})):i(s)}export{u as default,g as meta};
+`,e(n.p,{get children(){return["Once the binaries are installed, you will have to configure them to properly work with LSP, conform.nvim, nvim-lint, nvim-dap etc. It depends on what you installed. ",(()=>{var a=l(m);return h(a,e(n.strong,{children:"NvChad does not provide any language configuration aside from lua"})),a})(),"."]}}),`
+`]}})]}function g(s={}){const{wrapper:n}={...t(),...s.components};return n?e(n,o(s,{get children(){return e(i,s)}})):i(s)}export{g as default,u as meta};
