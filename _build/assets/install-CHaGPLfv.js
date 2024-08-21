d8ff3acf76b67f77a826e79dec0555ffa17ef835
siduck <siduck@tutanota.com>
2024-08-21 00:42:02 +0000

    Deploying to gh-pages from @ NvChad/nvchad.github.io@e10e4aff2f95cb39d617883312b7b3a477bf18c4 🚀

--- _build/assets/install-CHaGPLfv.js
+++ _build/assets/install-CHaGPLfv.js
@@ -24,7 +24,7 @@ import{b as e,q as s,k as t,t as l}from"./web-uzQsy4d_.js";import{M as a}from"./
 `,e(n.li,{get children(){return[e(n.code,{children:"Lazy sync"})," command"]}}),`
 `]}}),`
 `,e(n.h2,{children:"Uninstall"}),`
-`,e(n.pre,{get children(){return e(n.code,{className:"hljs language-bash",get children(){return[e(n.span,{className:"hljs-comment",children:"# Linux / Macos (unix)"}),`
+`,e(n.pre,{get children(){return e(n.code,{className:"hljs language-bash",get children(){return[e(n.span,{className:"hljs-comment",children:"# Linux / MacOS (unix)"}),`
 `,e(n.span,{className:"hljs-built_in",children:"rm"}),` -rf ~/.config/nvim
 `,e(n.span,{className:"hljs-built_in",children:"rm"}),` -rf ~/.local/state/nvim
 `,e(n.span,{className:"hljs-built_in",children:"rm"}),` -rf ~/.local/share/nvim
@@ -38,7 +38,7 @@ import{b as e,q as s,k as t,t as l}from"./web-uzQsy4d_.js";import{M as a}from"./
 rd -r ~\\AppData\\Local\\nvim
 rd -r ~\\AppData\\Local\\nvim-data
 
-`,e(n.span,{className:"hljs-comment",children:"# Window PowerShell"}),`
+`,e(n.span,{className:"hljs-comment",children:"# Windows PowerShell"}),`
 `,e(n.span,{className:"hljs-built_in",children:"rm"}),` -Force ~\\AppData\\Local\\nvim
 `,e(n.span,{className:"hljs-built_in",children:"rm"}),` -Force ~\\AppData\\Local\\nvim-data
 `]}})}})]}function w(r={}){const{wrapper:n}={...a(),...r.components};return n?e(n,s(r,{get children(){return e(i,r)}})):i(r)}export{w as default,N as meta};
