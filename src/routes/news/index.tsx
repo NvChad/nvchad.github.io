29d883538f1a62a6dd3eac21dfbb73629f6552ab
siduck <siduck@tutanota.com>
2024-09-11 13:55:07 +0530

    rm unused styles

--- src/routes/news/index.tsx
+++ src/routes/news/index.tsx
@@ -13,18 +13,14 @@ export const meta = {
 
 function News() {
   return (
-    <div m="y-5 xl:y-10 x-auto" px-3 max="w-[1700px]">
+    <div m="x-auto"  max="w-[1700px]" >
       {/* overview cards */}
-      <div grid gap-5 class="md:grid-cols-2 2xl:grid-cols-4">
+      <div grid gap5 class="md:grid-cols-2 2xl:grid-cols-4">
         {news.map((x) => (
-          <div
-            border="slate 0 dark:dark-4 solid"
-            class="shadow-lg flex flex-col  gap-4 items-start"
-            bg="dark:dark-3"
-          >
+          <div class=" flex flex-col items-start" bg="slate1 dark:dark-3">
             <img src={`/news/${x.cover}`} w-full rounded-t-lg loading="lazy" />
 
-            <div h-full flex flex-col gap-4 justify-between p-10 pt-5>
+            <div h-full flex flex-col gap-4 justify-between p-7>
               <div>
                 <h2 class="m-0" pb-5>
                   {x.title}
