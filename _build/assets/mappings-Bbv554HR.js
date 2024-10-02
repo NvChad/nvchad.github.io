f27da624001ea3f2c4c72dc281fee883b093964b
siduck <siduck@tutanota.com>
2024-10-02 16:24:54 +0000

    Deploying to gh-pages from @ NvChad/nvchad.github.io@185da3b49ecd433b64700b3d7c8b3fb5e1d4f2b2 🚀

--- _build/assets/mappings-Bbv554HR.js
+++ _build/assets/mappings-Bbv554HR.js
@@ -1,10 +1,10 @@
-import{b as e,q as t,k as a,t as l}from"./web-DqPol8Cv.js";import{M as c}from"./index-yHsRVhWw.js";var i=l("<kbd>Ctrl"),h=l("<kbd>Space"),d=l("<kbd>alt"),m=l("<kbd>shift"),p=l("<br>");const u={title:"NvChad Mappings",desc:"Manage mappings in nvchad"};function r(s){const n={a:"a",code:"code",h2:"h2",li:"li",p:"p",pre:"pre",span:"span",ul:"ul",...c(),...s.components};return[e(n.h2,{children:"Overview"}),`
+import{b as e,q as i,k as a,t as l}from"./web-DqPol8Cv.js";import{M as c}from"./index-yHsRVhWw.js";var t=l("<kbd>Ctrl"),h=l("<kbd>Space"),d=l("<kbd>alt"),p=l("<kbd>shift"),m=l("<br>");const u={title:"NvChad Mappings",desc:"Manage mappings in nvchad"};function r(s){const n={a:"a",code:"code",h2:"h2",li:"li",p:"p",pre:"pre",span:"span",ul:"ul",...c(),...s.components};return[e(n.h2,{children:"Overview"}),`
 `,e(n.p,{children:"The mapping configuration uses the nvim name shortcuts as:"}),`
 `,e(n.ul,{get children(){return[`
-`,e(n.li,{get children(){return[e(n.code,{children:"<C>"})," -> ",a(i)]}}),`
+`,e(n.li,{get children(){return[e(n.code,{children:"<C>"})," -> ",a(t)]}}),`
 `,e(n.li,{get children(){return[e(n.code,{children:"<leader>"})," -> ",a(h)]}}),`
 `,e(n.li,{get children(){return[e(n.code,{children:"<A>"})," -> ",a(d)]}}),`
-`,e(n.li,{get children(){return[e(n.code,{children:"<S>"})," -> ",a(m)]}}),`
+`,e(n.li,{get children(){return[e(n.code,{children:"<S>"})," -> ",a(p)]}}),`
 `,e(n.li,{get children(){return["The default mappings are defined ",e(n.a,{href:"https://github.com/NvChad/NvChad/blob/v2.5/lua/nvchad/mappings.lua",children:"here"}),"."]}}),`
 `]}}),`
 `,e(n.h2,{children:"Mapping format"}),`
@@ -31,8 +31,22 @@ map(`,e(n.span,{className:"hljs-string",children:'"n"'}),", ",e(n.span,{classNam
 nomap(`,e(n.span,{className:"hljs-string",children:'"i"'}),", ",e(n.span,{className:"hljs-string",children:'"<C-k>"'}),`)
 nomap(`,e(n.span,{className:"hljs-string",children:'"n"'}),", ",e(n.span,{className:"hljs-string",children:'"<C-k>"'}),`)
 `]}})}}),`
-`,a(p),`
+`,a(m),`
 `,e(n.ul,{get children(){return[`
 `,e(n.li,{children:"Do know that lsp mappings wont be overrided by the above methods, because they dont load on startup & are lazy loaded."}),`
-`,e(n.li,{get children(){return["So put the lsp ones in ",e(n.code,{children:"LspAttach"})," autocmd or right after your on_attach"]}}),`
-`]}})]}function N(s={}){const{wrapper:n}={...c(),...s.components};return n?e(n,t(s,{get children(){return e(r,s)}})):r(s)}export{N as default,u as meta};
+`,e(n.li,{children:"So put the lsp ones in a custom on_attach function"}),`
+`]}}),`
+`,e(n.pre,{get children(){return e(n.code,{className:"hljs language-lua",get children(){return[e(n.span,{className:"hljs-keyword",children:"local"})," ooo = ",e(n.span,{className:"hljs-function",get children(){return[e(n.span,{className:"hljs-keyword",children:"function"}),e(n.span,{className:"hljs-params",children:"(client, bufnr)"})]}}),`
+  nvlsp.on_attach(client, bufnr)
+  `,e(n.span,{className:"hljs-comment",children:"-- map HERE"}),`
+  vim.keymap.set(`,e(n.span,{className:"hljs-string",children:'"n"'}),", ",e(n.span,{className:"hljs-string",children:'"gd"'}),", ",e(n.span,{className:"hljs-string",children:'"<cmd> Telescope<cr>"'}),`, { buffer = bufnr })
+`,e(n.span,{className:"hljs-keyword",children:"end"}),`
+
+`,e(n.span,{className:"hljs-keyword",children:"for"})," _, lsp ",e(n.span,{className:"hljs-keyword",children:"in"})," ",e(n.span,{className:"hljs-built_in",children:"ipairs"}),"(servers) ",e(n.span,{className:"hljs-keyword",children:"do"}),`
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = ooo,
+    capabilities = nvlsp.capabilities,
+  }
+`,e(n.span,{className:"hljs-keyword",children:"end"}),`
+`]}})}})]}function N(s={}){const{wrapper:n}={...c(),...s.components};return n?e(n,i(s,{get children(){return e(r,s)}})):r(s)}export{N as default,u as meta};
