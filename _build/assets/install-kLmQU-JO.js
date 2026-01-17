320106544c39e26a40b77a379918064ce0db48ee
siduck <siduck@tutanota.com>
2026-01-17 17:37:56 +0000

    Deploying to gh-pages from @ NvChad/nvchad.github.io@7e1f4ead7f3d973afb3245272938f98ddf7d5c55 🚀

--- _build/assets/install-kLmQU-JO.js
+++ _build/assets/install-kLmQU-JO.js
@@ -1,13 +1,14 @@
-import{b as e,q as s,k as t,t as l}from"./web-DqPol8Cv.js";import{M as a}from"./index-yHsRVhWw.js";import{O as o}from"./install-C6Fa3z_f.js";import"./clipboard-DzJ4oxno.js";var c=l("<strong>Mono"),d=l("<strong>Example : "),h=l("<s>JetbrainsMono Nerd Font Mono"),m=l("<strong>(OPTIONAL)"),u=l("<br>");const w={title:"NvChad Installation",desc:"Install, Update, Uninstall guide for nvchad"};function i(r){const n={a:"a",code:"code",h2:"h2",li:"li",pre:"pre",span:"span",strong:"strong",ul:"ul",...a(),...r.components};return[e(n.h2,{children:"Pre-requisites"}),`
+import{b as e,q as s,k as t,t as l}from"./web-DqPol8Cv.js";import{M as a}from"./index-yHsRVhWw.js";import{O as o}from"./install-C6Fa3z_f.js";import"./clipboard-DzJ4oxno.js";var c=l("<strong>Mono"),h=l("<strong>Example : "),d=l("<s>JetbrainsMono Nerd Font Mono"),m=l("<strong>(OPTIONAL)"),u=l("<br>");const b={title:"NvChad Installation",desc:"Install, Update, Uninstall guide for nvchad"};function i(r){const n={a:"a",code:"code",h2:"h2",li:"li",pre:"pre",span:"span",strong:"strong",ul:"ul",...a(),...r.components};return[e(n.h2,{children:"Pre-requisites"}),`
 `,e(n.ul,{get children(){return[`
 `,e(n.li,{get children(){return[e(n.a,{href:"https://github.com/neovim/neovim/releases/tag/stable",children:"Neovim 0.11"}),"."]}}),`
 `,e(n.li,{get children(){return[e(n.a,{href:"https://www.nerdfonts.com/",children:"Nerd Font"})," as your terminal font.",`
 `,e(n.ul,{get children(){return[`
 `,e(n.li,{get children(){return["Make sure the nerd font you set doesn't end with ",t(c)," to prevent small icons."]}}),`
-`,e(n.li,{get children(){return[t(d)," JetbrainsMono Nerd Font and not ",e(n.strong,{get children(){return t(h)}})]}}),`
+`,e(n.li,{get children(){return[t(h)," JetbrainsMono Nerd Font and not ",e(n.strong,{get children(){return t(d)}})]}}),`
 `,e(n.li,{children:"The *Mono fonts would work too but icons will slightly look smaller."}),`
 `]}}),`
 `]}}),`
+`,e(n.li,{get children(){return[e(n.a,{href:"https://github.com/tree-sitter/tree-sitter/blob/master/crates/cli/README.md",children:"Tree-sitter-cli"})," is requied by nvim-treesitter plugin to install parsers."]}}),`
 `,e(n.li,{get children(){return[e(n.a,{href:"https://github.com/BurntSushi/ripgrep",children:"Ripgrep"})," is required for grep searching with Telescope ",t(m),"."]}}),`
 `,e(n.li,{get children(){return["GCC, Windows users must have ",e(n.a,{href:"http://mingw-w64.org/downloads",get children(){return e(n.code,{children:"mingw"})}})," installed and set on path."]}}),`
 `,e(n.li,{get children(){return["Make, Windows users must have ",e(n.a,{href:"https://sourceforge.net/projects/gnuwin32",get children(){return e(n.code,{children:"GnuWin32"})}})," installed and set on path."]}}),`
@@ -43,4 +44,4 @@ rd -r ~\\AppData\\Local\\nvim-data
 `,e(n.span,{className:"hljs-comment",children:"# Windows PowerShell"}),`
 `,e(n.span,{className:"hljs-built_in",children:"rm"}),` -Force ~\\AppData\\Local\\nvim
 `,e(n.span,{className:"hljs-built_in",children:"rm"}),` -Force ~\\AppData\\Local\\nvim-data
-`]}})}})]}function N(r={}){const{wrapper:n}={...a(),...r.components};return n?e(n,s(r,{get children(){return e(i,r)}})):i(r)}export{N as default,w as meta};
+`]}})}})]}function w(r={}){const{wrapper:n}={...a(),...r.components};return n?e(n,s(r,{get children(){return e(i,r)}})):i(r)}export{w as default,b as meta};
